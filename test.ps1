7ba7127ab85618374dea7efbc5e7185dc4159cc1
Vladyslav Horbachov <vhorbachov@microsoft.com>
2024-06-20 01:43:55 +0200

    Add New-PSsession check with basic authenication

--- test.ps1
+++ test.ps1
@@ -24,3 +24,8 @@ Enter-PSSession -Credential $CRED -ComputerName $IP -Authentication Basic -UseSS
 # $IP = '172.18.0.22'
 # $CRED = New-Object PSCredential $USERNAME, (ConvertTo-SecureString -String $PASSWORD -AsPlainText -Force)
 # Enter-PSSession -Credential $CRED -ComputerName $IP -Authentication Basic -UseSSL -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck)
+
+# $USERNAME = "User"; $PASSWORD = "Password12345!"; $IP = '172.18.0.22';
+# $credential = New-Object PSCredential $USERNAME, (ConvertTo-SecureString -String $PASSWORD -AsPlainText -Force)
+# $options = New-PSSessionOption -SkipCACheck -SkipCNCheck
+# New-PSSession -ComputerName '172.18.0.22' -Port 5986 -Authentication Basic -Name 'e7c9a24e-318f-488a-b513-56f419cb800c' -Credential $credential -UseSSL -SessionOption $options
